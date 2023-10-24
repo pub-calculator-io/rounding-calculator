b5931eaa5abe3195b3d3b940ff64e5d0776d76a5
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 15:55:03 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_rounding_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Rounding Calculator by Calculator.iO";
 
 function display_ci_rounding_calculator(){
